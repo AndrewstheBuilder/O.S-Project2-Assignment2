23a3b7fc68bd139f8f8c68d1e108ce67bd6ec3e3
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2022-03-18 16:20:07 -0400

    Added data structures for Bankers

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -1,13 +1,13 @@
-import java.util.LinkedList;
 import java.util.ArrayList;
 
 class Bankers{
     ArrayList<int[]> allocation;
-    ArrayList<int[]> available;
+    int[] available;
     ArrayList<int[]> need;
-    ArrayList<int[]> max;
-    Bankers(ArrayList<int[]> alloc, ArrayList<int[]> avail, ArrayList<int[]> max){
-
+    public Bankers(ArrayList<int[]> alloc, int[] avail, ArrayList<int[]> n){
+        this.allocation = alloc;
+        this.available = avail;
+        this.need = n;
     }
 }
 
@@ -24,9 +24,21 @@ public class Project2{
         alloc.add(p2);
         alloc.add(p3);
         alloc.add(p4);
-        for (int[] e : alloc) {
-            System.out.println(e[0]);
-        }
-
+        // for (int[] e : alloc) {
+        //     System.out.println(e[0]);
+        // }
+        ArrayList<int[]> n = new ArrayList<int[]>();
+        int[] n0 = {7,4,3};
+        int[] n1 = {0,2,0};
+        int[] n2 = {6,0,0};
+        int[] n3 = {0,1,1};
+        int[] n4 = {4,3,1};
+        n.add(n0);
+        n.add(n1);
+        n.add(n2);
+        n.add(n3);
+        n.add(n4);
+        int[] avail = {2,3,0};
+        Bankers b1 = new Bankers(alloc, avail, n);
     }
 }
